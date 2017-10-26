d21fd66f4fc84849310b37bf39bd80b901f69711
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-26 09:19:08 -0600

    more on assignment 5 -- ifs

--- js/index.js
+++ js/index.js
@@ -1,33 +1,92 @@
 $(document).ready(function(){
 //this line above always goes at the top//
 
-//input keywords and tags associated with photo
-//submit
-//update background image based on input (no browser refresh)
+//input city name
 
-//what goes here?
-var city = cityName
-
+	$("#submit-btn").click(changeImage);
+//DEFINE FUNCTION CALLED CHANGEIMAGE FUNCTION
 
 //enter keywords and tags 
-$('.#city-type').textField(keywordTags);
-function keywordTags();
+// $('.#text').textField(keywordTags);
+// function keywordTags();
 
+	function changeImage(){
+		event.preventDefault();
+	var input = $('#city-type').val();
+	console.log(input);
+	if (input == "New York" || input == "New York City" || input =="NYC"){
+//if(testsomething){then do whatever
+	$("body").addClass("nyc");
+	}
 
+	if (input == "San Francisco" || input =="SF" ||input == "Bay Area"){
+	//then show '#sf' image
+	$("body").addClass("sf");
+	}
 
-//when i click submit btn the phot switches
-$('#submit-btn').click(switchPhoto);
+	if (input == "Los Angeles" || input =="LA" || input =="LAX"){
+	//then show '#la' image
+	$("body").addClass("la");
+	}
 
-function switchPhoto(){
-	event.preventDefault();
+	if (input == "Austin" || input == "ATX"){
+	$("body").addClass("austin");
+	//then show '#austin' image
+	}
 
+	if (input == "Sydney" || input =="SYD"){
+	//then show '#sydney' image
+	$("body").addClass("sydney");
+	}
 
 }
 
-onclick.changeImage(this);
+//how to clear field once text is sumbitted -- should this be part of if else??
+$("#city-type").submit("Enter a city name...");
+//$("#submit").val("Enter a city name...");
+//input city name
+//- If a user submits:
+
+ 
+
+//update the image
+
+
+		//MY NOTES...
+		//use this to test each section--> alert("wrong");
+		//check on browser console for errors
+		//	event.preventDefault(); --> use this only for forms and links (eg show, hide, toggle)-- when the HTML takes you elsewhere by default; this stops it
+	
+	//clear out input
+
+
+	//listen for entry of city name
+	//look up code for a reset field
+
+
+//submit
+//update background image based on input (no browser refresh)
+
+
+
+
+
+
+
+// //when i click submit btn the phot switches
+// $('#submit-btn').click(switchPhoto);
+
+// function switchPhoto(){
+// 	event.preventDefault();
+
+
+// }
+
 
 // <script type="text/javascript">
 // function changeImage(a) {
 //     document.getElementById("img").src=a;
 // }
 // </script>
+
+});
