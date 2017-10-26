afd3cad5fe40b257387272883b5f2ee481d22709
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-25 20:46:19 -0600

    more on assigment

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+//this line above always goes at the top//
+
+//input keywords and tags associated with photo
+//submit
+//update background image based on input (no browser refresh)
+
+//what goes here?
+var city = cityName
+
+
+//enter keywords and tags 
+$('.#city-type').textField(keywordTags);
+function keywordTags();
+
+
+
+//when i click submit btn the phot switches
+$('#submit-btn').click(switchPhoto);
+
+function switchPhoto(){
+	event.preventDefault();
+
+
+}
+
+onclick.changeImage(this);
+
+// <script type="text/javascript">
+// function changeImage(a) {
+//     document.getElementById("img").src=a;
+// }
+// </script>
