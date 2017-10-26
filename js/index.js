0a3094cdef9ee5a21ceaa1df0be307afd1a5a0b4
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-26 10:44:37 -0600

    more changes - added submit-btn functionality

--- js/index.js
+++ js/index.js
@@ -1,51 +1,69 @@
 $(document).ready(function(){
 //this line above always goes at the top//
 
-//input city name
 
+
+//define function called changeImage
 	$("#submit-btn").click(changeImage);
-//DEFINE FUNCTION CALLED CHANGEIMAGE FUNCTION
 
-//enter keywords and tags 
-// $('.#text').textField(keywordTags);
-// function keywordTags();
+
+//input city name
 
 	function changeImage(){
 		event.preventDefault();
 	var input = $('#city-type').val();
-	console.log(input);
+
 	if (input == "New York" || input == "New York City" || input =="NYC"){
-//if(testsomething){then do whatever
+//if(testSomething){then then show '#nyc' image
 	$("body").addClass("nyc");
+	$("#submit-btn").click(changeImage);
+	//if not "New York", "New York City", or "NYC" show citipix skyline image
+	 } else {
+	 $ ("body").removeClass("nyc");
 	}
 
 	if (input == "San Francisco" || input =="SF" ||input == "Bay Area"){
 	//then show '#sf' image
 	$("body").addClass("sf");
+	$("#submit-btn").click(changeImage);
+	//if not "San Francisco", "SF", or "Bay Area" show citipix skyline image
+	 	} else {
+	 $ ("body").removeClass("sf");
 	}
 
 	if (input == "Los Angeles" || input =="LA" || input =="LAX"){
 	//then show '#la' image
 	$("body").addClass("la");
+	$("#submit-btn").click(changeImage);
+	//if not "Los Angeles", "LA", or "LAX" show citipix skyline image
+		} else {
+	 $ ("body").removeClass("la");
 	}
 
 	if (input == "Austin" || input == "ATX"){
-	$("body").addClass("austin");
 	//then show '#austin' image
+	$("body").addClass("austin");
+	$("#submit-btn").click(changeImage);
+	//if not "Austin", or "ATX" show citipix skyline image
+		} else {
+	 $ ("body").removeClass("austin");
 	}
 
 	if (input == "Sydney" || input =="SYD"){
 	//then show '#sydney' image
 	$("body").addClass("sydney");
+	$("#submit-btn").click(changeImage);
+	//if not "Sydney" or "SYD" show citipix skyline image
+		} else {
+	 $ ("body").removeClass("sydney");
 	}
 
 }
 
-//how to clear field once text is sumbitted -- should this be part of if else??
-$("#city-type").submit("Enter a city name...");
+//how to clear out input field once text is sumbitted -- should this be part of if else??
+$("#city-type").val("Enter a city name...");
+
 //$("#submit").val("Enter a city name...");
-//input city name
-//- If a user submits:
 
  
 
@@ -55,7 +73,7 @@ $("#city-type").submit("Enter a city name...");
 		//MY NOTES...
 		//use this to test each section--> alert("wrong");
 		//check on browser console for errors
-		//	event.preventDefault(); --> use this only for forms and links (eg show, hide, toggle)-- when the HTML takes you elsewhere by default; this stops it
+		//event.preventDefault(); --> use this only for forms and links (eg show, hide, toggle)-- when the HTML takes you elsewhere by default; this stops it
 	
 	//clear out input
 
