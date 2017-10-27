57631ecc7a3e58de53a016d4d5b5e6896c2e859c
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-27 10:12:40 -0600

    finished assignment 5

--- js/index.js
+++ js/index.js
@@ -1,110 +1,68 @@
 $(document).ready(function(){
 //this line above always goes at the top//
 
+//MY NOTES...
+//use this to test each section--> alert("wrong");
+//remember to check on browser console for errors
+//event.preventDefault(); --> use this only for forms and links (eg show, hide, toggle)-- when the HTML takes you elsewhere by default; this stops it
 
-
-//define function called changeImage
+	//define function called changeImage
 	$("#submit-btn").click(changeImage);
+	$("#city-type").focus(highlightText);
 
+//when click in field remove exisiting text
+	function highlightText(){
+		event.preventDefault();
+		$('#city-type').val("");
+	}
 
-//input city name
-
+	//input city name
 	function changeImage(){
 		event.preventDefault();
-	var input = $('#city-type').val();
+		var input = $('#city-type').val(); //fetch me whatever is put into the input field -- a new place/box to keep things
+		input = input.trim(); //called an assignment (DEFINITELY GOT SOME HELP WITH THIS ONE)
+		input = input.toUpperCase();//accept lower case -- make all inputs caps to work (DEFINITELY GOT SOME HELP WITH THIS ONE)
 
-	if (input == "New York" || input == "New York City" || input =="NYC"){
-//if(testSomething){then then show '#nyc' image
-	$("body").addClass("nyc");
-	$("#submit-btn").click(changeImage);
-	//if not "New York", "New York City", or "NYC" show citipix skyline image
-	 } else {
-	 $ ("body").removeClass("nyc");
-	}
-
-	if (input == "San Francisco" || input =="SF" ||input == "Bay Area"){
-	//then show '#sf' image
-	$("body").addClass("sf");
-	$("#submit-btn").click(changeImage);
-	//if not "San Francisco", "SF", or "Bay Area" show citipix skyline image
-	 	} else {
-	 $ ("body").removeClass("sf");
-	}
-
-	if (input == "Los Angeles" || input =="LA" || input =="LAX"){
-	//then show '#la' image
-	$("body").addClass("la");
-	$("#submit-btn").click(changeImage);
-	//if not "Los Angeles", "LA", or "LAX" show citipix skyline image
+		if (input == "NEW YORK" || input == "NEW YORK CITY" || input =="NYC"){
+			//if(testSomething){then show '#nyc' image
+			$("body").addClass("nyc");
+			//if not "New York", "New York City", or "NYC" show citipix skyline image
 		} else {
-	 $ ("body").removeClass("la");
-	}
+			 $ ("body").removeClass("nyc");
+		}
 
-	if (input == "Austin" || input == "ATX"){
-	//then show '#austin' image
-	$("body").addClass("austin");
-	$("#submit-btn").click(changeImage);
-	//if not "Austin", or "ATX" show citipix skyline image
+		if (input == "SAN FRANCISCO" || input == "SF" ||input == "BAY AREA"){
+			//then show '#sf' image
+			$("body").addClass("sf");
+			//if not "San Francisco", "SF", or "Bay Area" show citipix skyline image
+		 } else {
+		 $ ("body").removeClass("sf");
+		}
+
+		if (input == "LOS ANGELES" || input =="LA" || input =="LAX"){
+			//then show '#la' image
+			$("body").addClass("la");
+			//if not "Los Angeles", "LA", or "LAX" show citipix skyline image
 		} else {
-	 $ ("body").removeClass("austin");
-	}
+		 $ ("body").removeClass("la");
+		}
 
-	if (input == "Sydney" || input =="SYD"){
-	//then show '#sydney' image
-	$("body").addClass("sydney");
-	$("#submit-btn").click(changeImage);
-	//if not "Sydney" or "SYD" show citipix skyline image
+		if (input == "AUSTIN" || input == "ATX"){
+			//then show '#austin' image
+			$("body").addClass("austin");
+			//if not "Austin", or "ATX" show citipix skyline image
 		} else {
-	 $ ("body").removeClass("sydney");
+		 $ ("body").removeClass("austin");
+		}
+
+		if (input == "SYDNEY" || input =="SYD"){
+			//then show '#sydney' image
+			$("body").addClass("sydney");
+			//if not "Sydney" or "SYD" show citipix skyline image
+		} else {
+		 $ ("body").removeClass("sydney");
+		}
+		//how to clear out input field once text is sumbitted
+		$("#city-type").val("Enter a city name...");
 	}
-
-}
-
-//how to clear out input field once text is sumbitted -- should this be part of if else??
-$("#city-type").val("Enter a city name...");
-
-//$("#submit").val("Enter a city name...");
-
- 
-
-//update the image
-
-
-		//MY NOTES...
-		//use this to test each section--> alert("wrong");
-		//check on browser console for errors
-		//event.preventDefault(); --> use this only for forms and links (eg show, hide, toggle)-- when the HTML takes you elsewhere by default; this stops it
-	
-	//clear out input
-
-
-	//listen for entry of city name
-	//look up code for a reset field
-
-
-//submit
-//update background image based on input (no browser refresh)
-
-
-
-
-
-
-
-// //when i click submit btn the phot switches
-// $('#submit-btn').click(switchPhoto);
-
-// function switchPhoto(){
-// 	event.preventDefault();
-
-
-// }
-
-
-// <script type="text/javascript">
-// function changeImage(a) {
-//     document.getElementById("img").src=a;
-// }
-// </script>
-
-});
+})
